19b5213dfa228923e0e1c283d9350f589904a154
c21xdx <c21xdx@gmail.com>
2017-03-14 17:44:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
-/usr/local/bin/ssserver "ssserver -p 333 -k myss123 -m aes-256-cfb"
+ssserver -p 333 -k myss123 -m aes-256-cfb
